8f458deed5d139ebb955a84cc9298c9531ab9545
Juan <jpalenzuela@outlook.com>
2020-11-17 22:44:33 +0000

    advanced logic exercises

---
+++ advanced_logic.py
@@ -0,0 +1,37 @@
+# For the following list of numbers:
+
+numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
+
+# 1. Print out a list of the even integers:
+# 2. Print the difference between the largest and smallest value:
+# 3. Print True if the list contains a 2 next to a 2 somewhere.
+# 4. Print the sum of the numbers, 
+#    BUT ignore any section of numbers starting with a 6 and extending to the next 7.
+#    
+#    So [11, 6, 4, 99, 7, 11] would have sum of 22
+
+# 5. HARD! Print the sum of the numbers. 
+#    Except the number 13 is very unlucky, so it does not count.
+#    And numbers that come immediately after a 13 also do not count.
+#    HINT - You will need to track the index throughout the loop.
+#
+#    So [5, 13, 2] would have sum of 5. 
+
+# 1.
+for number in numbers:
+    if number % 2 == 0:
+        print(number)
+
+# 2. 
+smallest_value = min(numbers)
+largest_value = max(numbers)
+difference = largest_value - smallest_value
+
+# 3.
+print('2, 2' in str(numbers))
+
+# 4.
+six_index = numbers.index(6)
+seven_index = numbers.index(7)
+del numbers[six_index, seven_index]
+print(numbers)
